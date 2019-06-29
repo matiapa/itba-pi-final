6f497b9e2d3dc6ec51440a172d106ffab4a36703
inesmarca <imarcarian@itba.edu.ar>
2019-06-29 20:36:27 -0300

    Update test.c

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 int main(void) {
 	FILE * archEstacion;
 	archEstacion = fopen("estaciones.csv", "r");
+
 	unsigned int id;
 	char * linea = malloc(20);
 	char * estacion = malloc(25);
@@ -12,8 +13,15 @@ int main(void) {
 	trans = newTransporte();
 
 	int c;
+
+	/* Paso la primera linea de largo porque no me importa */
 	while ((c = fgetc(archEstacion)) != '\n');
+
+	/* Tomo la segunda linea */
 	fscanf(archEstacion, "%d,%[^,],%[^,]", &id, linea, estacion);
+
+	/* Agrego la estacion con los datos de la segunda linea */
 	addEstacion(trans, id, linea, estacion);
+
 	printf("%d, %s, %s", id, linea, estacion);
 }

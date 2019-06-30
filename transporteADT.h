5b35f76bf36c180c65b4c1717c586a933b855734
inesmarca <52361403+inesmarca@users.noreply.github.com>
2019-06-30 01:33:06 -0300

    Update transporteADT.h

--- transporteADT.h
+++ transporteADT.h
@@ -6,6 +6,8 @@ typedef struct transporteCDT * transporteADT;
 transporteADT newTransporte();
 void addEstacion(transporteADT trans, unsigned int id, char * nombre_linea, char * nombre_estacion);
 
+void addPasajero(transporteADT trans, unsigned int d, unsigned int m, unsigned int y, unsigned int hora, unsigned int id, unsigned int cant);
+
 // Solo para testing, borrar antes de entregar
 void getEstacion(transporteADT trans, int id, char *nombre, char *linea);
 

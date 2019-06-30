dfd136292567e0a6a66ee2e712c89e82537d2c6a
inesmarca <imarcarian@itba.edu.ar>
2019-06-30 19:30:12 -0300

    Create lectura.h

---
+++ lectura.h
@@ -0,0 +1,8 @@
+#ifndef __LECTURA_H_
+#define __LECTURA_H_
+
+void cargarEstaciones(transporteADT trans, char * archivo_estaciones);
+
+void cargarMolinetes(transporteADT trans, char * archivo_molinetes);
+
+#endif
